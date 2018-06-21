fd19d196a29281c4560d87bdec06cfedc7b2ffd9
SolenneD <sdaguerre75@gmail.com>
2018-06-21 13:30:30 +0200

    mail-envoi

--- contact.php
+++ contact.php
@@ -1,4 +1,66 @@
-<!DOCTYPE html>
+<?php
+
+//CONTACT
+
+$msg = '';
+//Don't run this unless we're handling a form submission
+if (array_key_exists('mail', $_POST)) {
+    date_default_timezone_set('Etc/UTC');
+
+    require 'PHPMailerAutoload.php';
+
+    //Create a new PHPMailer instance
+    $mail = new PHPMailer;
+    //Tell PHPMailer to use SMTP - requires a local mail server
+    //Faster and safer than using mail()
+    $mail->isSMTP();
+    $mail->SMTPAuth = true;
+    $mail->Host = 'smtp.gmail.com';
+    $mail->Username = "pacemecontact@gmail.com";
+    $mail->Password = "iesa2018!";
+    $mail->SMTPSecure = "ssl";
+    $mail->Port = 465;
+
+    //Use a fixed address in your own domain as the from address
+    //**DO NOT** use the submitter's address here as it will be forgery
+    //and will cause your messages to fail SPF checks
+    $mail->setFrom('pacemecontact@gmail.com', 'Pace Me');
+    //Send the message to yourself, or whoever should receive contact for submissions
+    $mail->addAddress('pacemecontact@gmail.com', '');
+    //Put the submitter's address in a reply-to header
+    //This will fail if the address provided is invalid,
+    //in which case we should ignore the whole request
+    if ($mail->addReplyTo($_POST['mail'], $_POST['nom'])) {
+    $mail->Subject = 'Prise de contact paceme.com';
+    //Keep it simple - don't use HTML
+    $mail->isHTML(false);
+
+
+    //Build a simple message body
+    $mail->Body = <<<EOT
+    
+            Professionnel/Particulier: {$_POST['pp']}
+                Name: {$_POST['name']}
+                Email: {$_POST['mail']}
+                Message: {$_POST['message']}
+
+EOT;
+    //Send the message, check for errors
+    if (!$mail->send()) {
+    //The reason for failing to send will be in $mail->ErrorInfo
+    //but you shouldn't display errors to users - process the error, log it on your server.
+    $msg = 'Désolé, une erreur est survenue. Veuillez réessayer plus tard';
+    } else {
+    $msg = 'Votre message a bien été envoyé !';
+    }
+    } else {
+    $msg = 'Adresse email invalide, message ignoré.';
+    }
+}
+
+
+
+?><!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="utf-8">
@@ -81,9 +143,12 @@
         <h2>Nous Contacter</h2>
         <p>Vous avez besoins de plus d’informations <br> ou bien vous désirez des réponses à vos questions ? <br>N’hésitez pas à nous contacter, nous sommes ici pour vous aider !</p>
 
-        <form class="">
+        <form method="post" action="envoi-mail.php">
+            <label for="name"></label>
             <input required type="text" name="name" id="name" placeholder="Nom">
-            <input required type="email" name="mail" id="email" placeholder="Adresse email">
+            <label for="email"></label>
+            <input required type="email" name="email" id="email" placeholder="Adresse email">
+            <label for="message"></label>
             <textarea required name="message" placeholder="Message" id="message" tabindex="4" cols="30" rows="8"></textarea>
 
             <div id="btn-checkbox">
